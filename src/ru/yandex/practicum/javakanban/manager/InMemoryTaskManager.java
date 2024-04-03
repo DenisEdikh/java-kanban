54afe7a9b9763ed8ca1e3f51eb1ddcbc1fa24182
Denis <denisedikh@yandex.ru>
2024-04-03 16:21:12 +0300

    переименован класс TaskManager в InMemoryTaskManager, реализующий интерфейс TaskManager

---
+++ src/ru/yandex/practicum/javakanban/manager/InMemoryTaskManager.java
@@ -0,0 +1,189 @@
+package ru.yandex.practicum.javakanban.manager;
+
+import ru.yandex.practicum.javakanban.model.Epic;
+import ru.yandex.practicum.javakanban.model.Status;
+import ru.yandex.practicum.javakanban.model.Subtask;
+import ru.yandex.practicum.javakanban.model.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InMemoryTaskManager implements TaskManager {
+
+    private int counterId = 0;
+    private HashMap<Integer, Task> tasks = new HashMap<>();
+    private HashMap<Integer, Epic> epics = new HashMap<>();
+    private HashMap<Integer, Subtask> subtasks = new HashMap<>();
+
+    @Override
+    public ArrayList<Task> getAllTasks() {
+        return new ArrayList<>(tasks.values());
+    } // получение списка всех задач
+
+    @Override
+    public void removeAllTasks() {
+        tasks.clear();
+    } // удаление всех задач
+
+    @Override
+    public Task getTaskById(int id) {
+        final Task task = tasks.get(id);
+        Managers.getDefaultHistory().addTaskToHistory(task);
+        return task;
+    } // получение задачи по идентификатору
+
+    @Override
+    public int addNewTask (Task task) {
+        task.setId(++counterId);
+        tasks.put(counterId, task);
+        return counterId;
+    } // добавление новой задачи
+
+    @Override
+    public void updateTask(Task task) {
+        if (tasks.containsKey(task.getId())) {
+            tasks.put(task.getId(), task);
+        }
+    } //обновление задачи
+
+    @Override
+    public void removeTaskById(int id) {
+        if (tasks.containsKey(id)) {
+            tasks.remove(id);
+        }
+    } //удаление по идентификатору
+
+    @Override
+    public ArrayList<Epic> getAllEpics() {
+        return new ArrayList<>(epics.values());
+    } // получение списка всех эпиков
+
+    @Override
+    public void removeAllEpic() {
+        epics.clear();
+        subtasks.clear();
+    } // удаление всех эпиков
+
+    @Override
+    public Epic getEpicById(int id) {
+        final Epic epic = epics.get(id);
+        Managers.getDefaultHistory().addTaskToHistory(epic);
+        return epic;
+    } // получение эпика по идентификатору
+
+    @Override
+    public int addNewEpic(Epic epic) {
+        epic.setId(++counterId);
+        epics.put(counterId, epic);
+        return counterId;
+    } //добавление нового эпика
+
+    @Override
+    public void updateEpic(Epic epic) {
+        if (epics.containsKey(epic.getId())) {
+            final Epic savedEpic = epics.get(epic.getId());
+            savedEpic.setTitle(epic.getTitle());
+            savedEpic.setDescription(epic.getDescription());
+        }
+    } // обновление эпика
+
+    @Override
+    public void removeEpicById(int id) {
+        if (epics.containsKey(id)) {
+            for (Integer i : epics.get(id).getSubtaskIdS()) {
+                subtasks.remove(i);
+            }
+            epics.remove(id);
+        }
+    } // удаление эпика по идентификатору
+
+    @Override
+    public ArrayList<Subtask> getEpicSubtasks(int epicId) {
+        ArrayList<Subtask> epicSubtask = new ArrayList<>();
+        if (epics.containsKey(epicId)) {
+            for (Integer i : epics.get(epicId).getSubtaskIdS()) {
+                epicSubtask.add(subtasks.get(i));
+            }
+        }
+        return epicSubtask;
+    } //получение списка подзадач определенного эпика
+
+    @Override
+    public ArrayList<Subtask> getAllSubtask() {
+        return new ArrayList<>(subtasks.values());
+    } // получение списка всех подзадач
+
+    @Override
+    public void removeAllSubtask() {
+        subtasks.clear();
+        for (Epic value : epics.values()) {
+            value.clearSubtaskIdS();
+            updateStatusOfEpic(value.getId());
+        }
+    } // удаление всех подзадач
+
+    @Override
+    public Subtask getSubtaskById(int id) {
+        final Subtask subtask = subtasks.get(id);
+        Managers.getDefaultHistory().addTaskToHistory(subtask);
+        return subtask;
+    } // получение подзадачи по идентификатору
+
+    @Override
+    public int addNewSubtask(Subtask subtask) {
+        if (epics.containsKey(subtask.getEpicId())) {
+            subtask.setId(++counterId);
+            epics.get(subtask.getEpicId()).addSubtaskId(counterId);
+            subtasks.put(counterId, subtask);
+            updateStatusOfEpic(subtask.getEpicId());
+            return counterId;
+        }
+        return -1;
+    } //добавление подзадачи
+
+    @Override
+    public void updateSubTask(Subtask subtask) {
+        if (epics.containsKey(subtask.getEpicId()) &
+                epics.get(subtask.getEpicId()).getSubtaskIdS().contains(subtask.getId())) {
+            subtasks.put(subtask.getId(), subtask);
+            updateStatusOfEpic(subtask.getEpicId());
+        }
+    } //обновление подзадачи
+
+    @Override
+    public void removeSubtaskById(int id) {
+        if (subtasks.containsKey(id)) {
+            int tempEpicId = subtasks.get(id).getEpicId();
+            epics.get(tempEpicId).removeSubtaskId(id);
+            subtasks.remove(id);
+            updateStatusOfEpic(tempEpicId);
+        }
+    } // удаление подзадачи по идентификатору
+
+    private void updateStatusOfEpic(int id) {
+        Epic tempEpic = epics.get(id);
+        if (tempEpic.getSubtaskIdS().isEmpty()) {
+            tempEpic.setStatus(Status.NEW);
+        } else {
+            int counterNew = 0;
+            int counterDone = 0;
+
+            for (Integer i : tempEpic.getSubtaskIdS()) {
+                if (subtasks.get(i).getStatus().equals(Status.NEW)) { //потом поменять местами переменные
+                    counterNew++;
+                } else if (subtasks.get(i).getStatus().equals(Status.DONE)) {
+                    counterDone++;
+                }
+            }
+            if (counterNew == tempEpic.getSubtaskIdS().size()) {
+                tempEpic.setStatus(Status.NEW);
+            } else if (counterDone == tempEpic.getSubtaskIdS().size()) {
+                tempEpic.setStatus(Status.DONE);
+            } else {
+                tempEpic.setStatus(Status.IN_PROGRESS);
+            }
+        }
+    } // обновление статуса эпика
+
+}
+
