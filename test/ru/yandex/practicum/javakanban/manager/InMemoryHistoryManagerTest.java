ebbed8aa49dae517a5a23e73f9ade6395d001608
Denis <denisedikh@yandex.ru>
2024-04-23 18:57:01 +0300

    Откорректировать класс InMemoryHistoryManagerTest: 1. Добавить метод shouldTaskBeMissingAfterDeletion() для проверки отсутствия задачи в списки просмотренных. 2. Добавить метод shouldReturnCorrectLinkWhenFormingLinkedList() для проверки получения связного списка. 3. Откорректировать метод shouldReturn10TaskAndDeleteFirstTaskAndAdd11Task() на соответствие последовательного добавления задач.

--- test/ru/yandex/practicum/javakanban/manager/InMemoryHistoryManagerTest.java
+++ test/ru/yandex/practicum/javakanban/manager/InMemoryHistoryManagerTest.java
@@ -1,37 +1,71 @@
 package ru.yandex.practicum.javakanban.manager;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 import ru.yandex.practicum.javakanban.model.Task;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InMemoryHistoryManagerTest {
-   HistoryManager historyManager = new InMemoryHistoryManager();
-   @Test
+    InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+    @Test
     void shouldReturn10TaskAndDeleteFirstTaskAndAdd11Task() {
-       Task task = new Task("test addTask", "test addTask description");
-       Task task1 = new Task("test addNewTask", "test addNewTask description");
+        Task task = new Task("test addTask", "test addTask description");
+        task.setId(1);
+        Task task1 = new Task("test addNewTask", "test addNewTask description");
+        task1.setId(2);
 
-       historyManager.addTaskToHistory(task1);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task);
-       historyManager.addTaskToHistory(task1);
+        historyManager.addTaskToHistory(task1);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task);
+        historyManager.addTaskToHistory(task1);
 
-       final List<Task> savedHistory = historyManager.getHistory();
-       final int sizeOfHistory = savedHistory.size();
+        final List<Task> savedHistory = historyManager.getHistory();
+        final int sizeOfHistory = savedHistory.size();
 
-       assertNotNull(savedHistory, "История не пустая");
-       assertEquals(10, sizeOfHistory, "История не пустая");
-       assertEquals(task1, savedHistory.get(9), "Неправильное хранение истории");
+        assertNotNull(savedHistory, "История пустая");
+        assertEquals(2, sizeOfHistory, "История пустая");
+        assertEquals(task, savedHistory.get(0), "Неверная запись по порядку");
+        assertEquals(task1, savedHistory.get(1), "Неверная запись по порядку");
 
-   }
+    }
+
+    @Test
+    void shouldReturnCorrectLinkWhenFormingLinkedList() {
+        Task task = new Task("test addTask", "test addTask description");
+        Task task1 = new Task("test addNewTask", "test addNewTask description");
+
+        historyManager.linkLast(task);
+        assertNotNull(historyManager.getHead(), "Некорректная ссылка");
+        assertNotNull(historyManager.getTail(), "Некорректная ссылка");
+
+        historyManager.linkLast(task1);
+        historyManager.linkLast(task1);
+        assertNull(historyManager.getHead().getPrev(), "Некорректная ссылка");
+        assertNull(historyManager.getTail().getNext(), "Некорректная ссылка");
+        assertNotNull(historyManager.getHead().getNext(), "Некорректная ссылка");
+        assertEquals(historyManager.getHead().getNext(), historyManager.getTail().getPrev(),
+                "Несоответствие ссылок");
+    }
+
+    @Test
+    void shouldTaskBeMissingAfterDeletion() {
+        Task task = new Task("test addTask", "test addTask description");
+        Task task1 = new Task("test addNewTask", "test addNewTask description");
+
+        historyManager.addTaskToHistory(task1);
+        historyManager.addTaskToHistory(task);
+        historyManager.remove(0);
+
+        assertFalse(historyManager.getHistory().contains(task1), "Запись найдена");
+    }
 }
