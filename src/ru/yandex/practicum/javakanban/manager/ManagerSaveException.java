70c8f129b9a2b46c9b06c8ee07f8766df07afcea
Denis <denisedikh@yandex.ru>
2024-05-08 19:45:16 +0300

    Добавить конструктор с сообщением об ошибке

--- src/ru/yandex/practicum/javakanban/manager/ManagerSaveException.java
+++ src/ru/yandex/practicum/javakanban/manager/ManagerSaveException.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.javakanban.manager;
 
 public class ManagerSaveException extends RuntimeException {
-
+    public ManagerSaveException() {
+        System.out.println("Файл отсутствует!");
+    }
 }
