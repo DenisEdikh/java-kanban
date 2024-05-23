ca6a21ac0b36592d2ac79be9ee88a4b5836858ad
Denis <denisedikh@yandex.ru>
2024-05-23 18:19:17 +0300

    1. Добавить тест на проверку пустой истории 2. Откорректировать тест shouldTaskBeMissingAfterDeletion() на корректность удаления из начала истории, середины.

--- test/ru/yandex/practicum/javakanban/manager/InMemoryHistoryManagerTest.java
+++ test/ru/yandex/practicum/javakanban/manager/InMemoryHistoryManagerTest.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.javakanban.manager;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.javakanban.model.Task;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +13,29 @@ public class InMemoryHistoryManagerTest {
     InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
 
     @Test
-    void shouldReturn10TaskAndDeleteFirstTaskAndAdd11Task() {
-        Task task = new Task("test addTask", "test addTask description");
+    void shouldEmptyHistory() {
+        Task task = new Task("Task",
+                "Task description",
+                null,
+                10);
         task.setId(1);
-        Task task1 = new Task("test addNewTask", "test addNewTask description");
+        historyManager.addTaskToHistory(task);
+        historyManager.remove(task.getId());
+        int sizeHistoryManager = historyManager.getHistory().size();
+
+        assertEquals(0, sizeHistoryManager, "История не пустая");
+    }
+    @Test
+    void shouldReturn10TaskAndDeleteFirstTaskAndAdd11Task() {
+        Task task = new Task("Task",
+                "Task description",
+                null,
+                10);
+        task.setId(1);
+        Task task1 = new Task("Task1",
+                "Task1 description",
+                LocalDateTime.of(2025, Month.MAY, 22, 16, 30),
+                10);
         task1.setId(2);
 
         historyManager.addTaskToHistory(task1);
@@ -32,6 +53,7 @@ public class InMemoryHistoryManagerTest {
         final List<Task> savedHistory = historyManager.getHistory();
         final int sizeOfHistory = savedHistory.size();
 
+        // Проверка на отсутствие дублирования
         assertNotNull(savedHistory, "История пустая");
         assertEquals(2, sizeOfHistory, "История пустая");
         assertEquals(task, savedHistory.get(0), "Неверная запись по порядку");
@@ -41,8 +63,14 @@ public class InMemoryHistoryManagerTest {
 
     @Test
     void shouldReturnCorrectLinkWhenFormingLinkedList() {
-        Task task = new Task("test addTask", "test addTask description");
-        Task task1 = new Task("test addNewTask", "test addNewTask description");
+        Task task = new Task("Task",
+                "Task description",
+                null,
+                10);
+        Task task1 = new Task("Task1",
+                "Task1 description",
+                LocalDateTime.of(2025, Month.MAY, 22, 16, 30),
+                10);
 
         historyManager.linkLast(task);
         assertNotNull(historyManager.getHead(), "Некорректная ссылка");
@@ -59,13 +87,35 @@ public class InMemoryHistoryManagerTest {
 
     @Test
     void shouldTaskBeMissingAfterDeletion() {
-        Task task = new Task("test addTask", "test addTask description");
-        Task task1 = new Task("test addNewTask", "test addNewTask description");
+        Task task = new Task("Task",
+                "Task description",
+                null,
+                10);
+        task.setId(1);
+        Task task1 = new Task("Task1",
+                "Task1 description",
+                LocalDateTime.of(2025, Month.MAY, 22, 16, 30),
+                10);
+        task1.setId(2);
+        Task task2 = new Task("Task2",
+                "Task2 description",
+                LocalDateTime.of(2023, Month.MAY, 22, 16, 30),
+                10);
+        task2.setId(3);
 
         historyManager.addTaskToHistory(task1);
         historyManager.addTaskToHistory(task);
-        historyManager.remove(0);
+        historyManager.remove(task1.getId());
 
+        // Удаление из начала
         assertFalse(historyManager.getHistory().contains(task1), "Запись найдена");
+
+        historyManager.addTaskToHistory(task1);
+        historyManager.addTaskToHistory(task2);
+        historyManager.remove(task1.getId());
+
+        // Удаление из конца
+        assertFalse(historyManager.getHistory().contains(task1), "Запись найдена");
+
     }
 }
