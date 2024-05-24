ba2de3cfca7e8051bb9c245e8b9f4030ce5e2efd
Denis <denisedikh@yandex.ru>
2024-05-24 09:42:13 +0300

    1. Добавлено непроверяемое исключение, выбрасываемое при наложении времени выполнения задач

---
+++ src/ru/yandex/practicum/javakanban/manager/ManagerTimeException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.javakanban.manager;
+
+public class ManagerTimeException extends RuntimeException {
+    public ManagerTimeException(String message) {
+        super(message);
+    }
+}
