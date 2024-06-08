a5fff88c1988a6153333a409d378ab943eb1f939
Denis <denisedikh@yandex.ru>
2024-06-08 21:08:41 +0300

    1. Убрать метод getGson() получения экземпляра Gson в класс BaseHttpHandler

--- src/ru/yandex/practicum/javakanban/manager/Managers.java
+++ src/ru/yandex/practicum/javakanban/manager/Managers.java
@@ -1,13 +1,5 @@
 package ru.yandex.practicum.javakanban.manager;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import ru.yandex.practicum.javakanban.adapter.DurationAdapter;
-import ru.yandex.practicum.javakanban.adapter.LocalDateTimeAdapter;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-
 public final class Managers {
 
     public static HistoryManager getDefaultHistory() {
@@ -17,13 +9,5 @@ public final class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
-
-    public static Gson getGson() {
-        return new GsonBuilder()
-                .setPrettyPrinting()
-                .serializeNulls()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
-                .registerTypeAdapter(Duration.class, new DurationAdapter())
-                .create();
-    }
 }
+
