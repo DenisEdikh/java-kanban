3ca724e35d155874239cb9482156333f2685ce3b
Denis <denisedikh@yandex.ru>
2024-06-08 19:05:08 +0300

    1. Откорректировать CodeStyle

--- src/ru/yandex/practicum/javakanban/exception/CreateJsonException.java
+++ src/ru/yandex/practicum/javakanban/exception/CreateJsonException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.javakanban.exception;
 
-public class CreateJsonException extends RuntimeException{
+public class CreateJsonException extends RuntimeException {
     public CreateJsonException(String message) {
         super(message);
     }
