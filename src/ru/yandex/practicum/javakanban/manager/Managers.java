08e1333b32e9138466f63f212f4aa4fa51009a10
Denis <denisedikh@yandex.ru>
2024-06-08 18:43:52 +0300

    1. Добавить статический метод вызова экземпляра Gson по умолчанию

--- src/ru/yandex/practicum/javakanban/manager/Managers.java
+++ src/ru/yandex/practicum/javakanban/manager/Managers.java
@@ -1,5 +1,13 @@
 package ru.yandex.practicum.javakanban.manager;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.yandex.practicum.javakanban.adapter.DurationAdapter;
+import ru.yandex.practicum.javakanban.adapter.LocalDateTimeAdapter;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public final class Managers {
 
     public static HistoryManager getDefaultHistory() {
@@ -8,5 +16,14 @@ public final class Managers {
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
+    }
+
+    public static Gson getGson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(Duration.class, new DurationAdapter())
+                .create();
     }
 }
